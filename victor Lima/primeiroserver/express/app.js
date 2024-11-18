058bd7cb99ec5db31cf52040985062ce517e4b62
Dannick010 <danmaggot2@gmail.com>
2024-11-18 15:40:14 -0300

    requests

--- victor Lima/primeiroserver/express/app.js
+++ victor Lima/primeiroserver/express/app.js
@@ -10,8 +10,17 @@ app.get("/sobre", (req, res) => {
 });
 
 app.get("/blog", (req, res) => {
-    res.send("blog")
+    res.send("blog");
+});
+
+app.get('/ola/:nome/:idade', (req, res) => {
+    res.send(`<h1>Olá ${req.params.nome} </h1>, <h2>proximo ano voçê faz ${aumentaridade(req.params.idade)}</h2> <p>Busque o oftamologista</p>`)
 })
+
+function aumentaridade(idade) {
+    let num = parseInt(idade)
+    return num +1
+}
 
 app.listen(8081, () => {
     console.log("servidor Rodando na URL http://localhost:8081")
