a71f7890871b5ef53a3275e4e42726439bc96475
Dannick010 <danmaggot2@gmail.com>
2024-11-18 20:48:14 -0300

    handlebars

--- victor Lima/primeiroserver/firstproject/app.js
+++ victor Lima/primeiroserver/firstproject/app.js
@@ -1,9 +1,18 @@
 const express = require("express")
-
 const app = express()
+const handlebars = require('express-handlebars')
+const Sequelize = require("sequelize")
 
-app.get("/", (req,res) => {
-    res.send("BIRL")
+
+// config 
+// template Engine 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+// conexão com o banco de dados Mysql
+const sequelize = new Sequelize('teste', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
 })
+
 
 app.listen(8081)
