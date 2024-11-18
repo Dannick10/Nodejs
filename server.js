caecab6de943c0399fe598e0726da4068c221b13
Dannick010 <danmaggot2@gmail.com>
2024-11-18 13:57:23 -0300

    criando server

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("testando")
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+let route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "node store",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+server.listen(port)
+console.log("APi rodando porta")
