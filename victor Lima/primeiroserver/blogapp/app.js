7b1bb9fee6c862ae1a6d568931409729a5985dcd
Dannick010 <danmaggot2@gmail.com>
2024-11-29 15:40:10 -0300

    model e add categorias

--- victor Lima/primeiroserver/blogapp/app.js
+++ victor Lima/primeiroserver/blogapp/app.js
@@ -1,24 +1,29 @@
 // Carregando modulos
 const express = require("express");
 const { engine } = require("express-handlebars");
-const bodyParser = require("body-parser");
 const app = express();
 const admin = require("./routes/admin");
-const path = require("path")
-//const mongoose = require('mongoose)
+const path = require("path");
+const { mongo } = require("mongoose");
+const mongoose = require('mongoose')
 
 //config
 
 //boddy parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json())    // para ler json
+app.use(express.urlencoded({ extended: true}))
 
 //handlebars
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 //mongoose
-//em breve
+ mongoose.connect('mongodb://localhost/blogapp')
+ .then(() => {
+  console.log('conectado ao mongodb')
+ }).catch((error) => {
+  console.lodg('erro ao conectar' + error)
+ } )
 //public 
 app.use(express.static(path.join(__dirname,"public")))
 //rotas
--- victor Lima/primeiroserver/blogapp/routes/admin.js
+++ victor Lima/primeiroserver/blogapp/routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categorias = mongoose.model('categorias')
 router.get('/', (req,res) => {
     res.render('admin/index')
 })
@@ -15,6 +17,23 @@ router.get('/categorias', (req,res) => {
 
 router.get('/categorias/add', (req,res) => {
     res.render('admin/addcategorias')
+})
+
+router.post("/categorias/nova", (req,res) => {
+    const novaCategoria = {
+        name: req.body.name,
+        slug: req.body.slug
+    }
+
+    console.log(novaCategoria)
+
+    new Categorias(novaCategoria).save()
+    .then(()=> {
+        console.log('categoria salva')
+    })
+    .catch((error) => {
+        console.log('error ao salvar categoria' + error)
+    })
 })
 
 module.exports = router
