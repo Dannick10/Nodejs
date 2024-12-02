39780661cc31a2a6effc143edbdfd8a1b882074e
Dannick010 <danmaggot2@gmail.com>
2024-12-02 13:12:33 -0300

    validando formulario

--- victor Lima/primeiroserver/blogapp/routes/admin.js
+++ victor Lima/primeiroserver/blogapp/routes/admin.js
@@ -1,39 +1,58 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-require('../models/Categoria')
-const Categorias = mongoose.model('categorias')
-router.get('/', (req,res) => {
-    res.render('admin/index')
-})
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria");
+const Categorias = mongoose.model("categorias");
+router.get("/", (req, res) => {
+  res.render("admin/index");
+});
 
-router.get('/posts', (req,res) => {
-    res.send('posts')
-})
+router.get("/posts", (req, res) => {
+  res.send("posts");
+});
 
-router.get('/categorias', (req,res) => {
-    res.render('admin/categorias')
-})
+router.get("/categorias", (req, res) => {
+  res.render("admin/categorias");
+});
 
-router.get('/categorias/add', (req,res) => {
-    res.render('admin/addcategorias')
-})
+router.get("/categorias/add", (req, res) => {
+  res.render("admin/addcategorias");
+});
 
-router.post("/categorias/nova", (req,res) => {
+router.post("/categorias/nova", (req, res) => {
+  let errors = [];
+
+  if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+    errors.push({ text: "Nome invalido" });
+  } 
+  
+  if (req.body.name.length < 2) {
+    errors.push({ text: "Nome nao pode ser menor que 2" });
+  }
+
+  if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+    errors.push({ text: "Slug invalido" });
+  }
+
+  if (errors.length > 0) {
+    res.render("admin/addcategorias", { errors: errors });
+  } else {
     const novaCategoria = {
-        name: req.body.name,
-        slug: req.body.slug
-    }
+      nome: req.body.name,
+      slug: req.body.slug,
+    };
 
-    console.log(novaCategoria)
+    new Categorias(novaCategoria)
+      .save()
+      .then(() => {
+        req.flash('sucess_msg', 'Categoria criada com sucesso');
+        res.redirect('/admin/categorias');
+      })
+      .catch((error) => {
+        req.flash('error_msg', 'Erro ao salvar categoria');
+        res.redirect("/admin");
+      });
+  }
+});
 
-    new Categorias(novaCategoria).save()
-    .then(()=> {
-        console.log('categoria salva')
-    })
-    .catch((error) => {
-        console.log('error ao salvar categoria' + error)
-    })
-})
-
-module.exports = router
+module.exports = router;
