9503734531e91545b301a265e74889f0262d4632
Dannick010 <danmaggot2@gmail.com>
2025-01-02 15:45:46 -0300

    teste

--- victor Lima/primeiroserver/blogapp/routes/usuario.js
+++ victor Lima/primeiroserver/blogapp/routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const bycript = require("bcryptjs")
 
 router.get("/registro", (req,res) => {
     res.render("usuarios/registro") 
@@ -35,7 +36,41 @@ router.post("/registro", (req,res) => {
     if(erros.length > 0) {
         res.render("usuarios/registro", {erros: erros})
     } else {
+        Usuario.findOne({email: req.body.email}).then((usuario) => {
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este email no nosso sistema")
+                res.redirect("/usuarios/registro")
+            }else {
 
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+                bycript.genSalt(10, (erro,salt) => {
+                    bycript.hash(novoUsuario.senha, salt, (erro,hash) => {
+                        if(erro){
+                            req.flash("error_msg", "Houve um erro durante o salvamento do usuário")
+                            res.redirect("/")
+                        }
+        
+                        novoUsuario.senha = hash
+        
+                        novoUsuario.save().then(() => {
+                            req.flash("sucess_msg", "Usuário criado com sucesso")
+                            res.redirect("/")
+                        }).catch((erro) => {
+                            req.flash("error_msg", "Houve um erro ao criar o usuário, tente novamente")
+                            res.redirect("/usuarios/registro")
+                        })
+                    })
+                })
+            }
+        }).catch((erro) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
     }
 
 })
